594d76943cd247e497cbb1b8f44e8be33cdd769e
Steven Rosa <s.rosa21@icloud.com>
2021-12-03 19:18:06 -0500

    MVP achieved

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,37 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
+    render(<CheckoutForm />)
+});
 
-test("shows success message on submit with form details", () => {});
+test("shows success message on submit with form details", async () => {
+    render(<CheckoutForm />);
+
+    const firstName = screen.getByLabelText('First Name:');
+    const lastName = screen.getByLabelText('Last Name:');
+    const address = screen.getByLabelText('Address:');
+    const city = screen.getByLabelText('City:');
+    const state = screen.getByLabelText('State:');
+    const zip = screen.getByLabelText('Zip:');
+    const checkoutButton = screen.getByText('Checkout');
+    const nullSuccessMsg = screen.queryByTestId('successMessage');
+
+    expect(nullSuccessMsg).not.toBeInTheDocument();
+
+    userEvent.type(firstName, "John");
+    userEvent.type(lastName, "Doe");
+    userEvent.type(address, "1234 Street st");
+    userEvent.type(city, "New York City");
+    userEvent.type(state, "NY");
+    userEvent.type(zip, "12345");
+    userEvent.click(checkoutButton);
+    
+    const successMsg = await screen.queryByTestId('successMessage'); 
+    expect(successMsg).toBeInTheDocument();
+});
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,10 @@
+import { useState } from "react"
+
 // write your custom hook here to control your checkout form
+export const useForm = initialValue => {
+    const [value, setValue] = useState(initialValue);
+    const handleChanges = updatedValue => {
+        setValue(updatedValue);
+    };
+    return [value, setValue, handleChanges];
+}
